150968c05dd460c3c88aec3c47ce477c8ea5051c
aanvarov <aanvarov@evolutiongaming.com>
2020-03-07 12:42:46 +0200

    add main page & add style to index js

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,23 @@
-import React from 'reqact'
+import React from 'react'
+import './main.css'
 
 
 
 export const MainPage = () => {
-
-
-
+  return (
+    <div className={"container-fluid main-page  h-100"}>
+      <div className="row h-100">
+        <div className="col h-100">
+          <div className="colContentWrapper">
+            One of three columns
+          </div>
+        </div>
+        <div className="col h-100">
+          <div className="colContentWrapper">
+            One of three columns
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
