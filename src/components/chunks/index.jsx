1f37683549b765cb79898ab4ba830f1b0d7af4f7
Karl-Gustav Kallasmaa <karl.gustav1789@gmail.com>
2020-03-07 16:25:53 +0200

    iframe

---
+++ src/components/chunks/index.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
+
+export default class MasterChunk extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      treeData: [
+        { title: 'Title one', children: [{ title: 'sub 1' }] },
+        { title: 'Title two', children: [{ title: 'sub 2' }] },
+      ],
+    };
+    navigator.clipboard.readText()
+      .then(text => {
+        alert(text)
+      })
+  }
+
+  render() {
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree treeData={this.state.treeData} onChange={(treeData) => this.setState({ treeData })} />
+      </div>
+    );
+  }
+}
