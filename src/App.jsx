9d773e76a91b104af91eaf24a834f80fa95a4fc9
Karl-Gustav Kallasmaa <karl.gustav1789@gmail.com>
2020-03-07 12:43:24 +0200

    Fixing typescript (#2)
    * fixing typecript bug
    
    * adding CI
    
    * fixing one thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {MainPage} from "./pages/main";
+import { MainPage } from './pages/main';
 
-export const App: React.FC = () => {
-  return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-        </Switch>
-      </Router>
-  );
+
+export const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={MainPage} />
+            </Switch>
+        </Router>
+    );
 };
