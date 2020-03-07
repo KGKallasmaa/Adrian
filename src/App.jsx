195f725945a465c08ebbce7c4292957521bb8eae
Karl-Gustav Kallasmaa <karl.gustav1789@gmail.com>
2020-03-07 14:17:17 +0200

    inital bot

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { MainPage } from './pages/main';
+import { InitalBotcreator } from './pages/inital';
 
-
-export const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={MainPage} />
-            </Switch>
-        </Router>
-    );
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/bot" component={InitalBotcreator} />
+        <Route exact path="/" component={MainPage} />
+      </Switch>
+    </Router>
+  );
 };
+export default App;
---
+++ src/pages/inital/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import './assets/css/main.css';
+import './assets/css/util.css';
+
+import logo from './assets/images/logo.png';
+
+export class InitalBotcreator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Adrian',
+      welcome_message: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+  handleClick(e) {
+    e.preventDefault();
+    const { name, welcome_message } = this.state;
+    //TODO: what should we do with the info?
+    //Currently redirecting to the main page
+    window.location.href = '/';
+  }
+
+  render() {
+    const name = this.state.name;
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Welcome to Adrian</title>
+        </Helmet>
+        <div className="limiter">
+          <div className="container-login100">
+            <div className="wrap-login100">
+              <form className="login100-form validate-form">
+                <span className="login100-form-title p-b-26">{name} </span>
+                <span className="login100-form-title p-b-48">
+                  <img className="logo" src={logo} alt={'Adrian logo'} />
+                </span>
+                <div className="wrap-input100 validate-input">
+                  <input
+                    className="input100"
+                    type="text"
+                    name="name"
+                    onChange={this.handleChange}
+                    placeholder={'Name'}
+                  />
+                </div>
+                <div className="wrap-input100 validate-input">
+                  <input
+                    className="input100"
+                    type="text"
+                    name="welcome_message"
+                    onChange={this.handleChange}
+                    placeholder={'Welcoming message'}
+                  />
+                </div>
+                <div className="container-login100-form-btn">
+                  <div className="wrap-login100-form-btn">
+                    <div className="login100-form-bgbtn" />
+                    <button className="login100-form-btn" onClick={this.handleClick}>
+                      Create bot
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div id="dropDownSelect1" />
+      </React.Fragment>
+    );
+  }
+}
